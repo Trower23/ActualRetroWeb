2fc2ae4ed359989582b7218463686d43364a0452
José Luis <joseluis06052002@gmail.com>
2022-03-05 16:39:57 +0100

    He actualizado y añadido varias cosas

--- src/main/java/com/dws/ActualRetro/ShoppingCart.java
+++ src/main/java/com/dws/ActualRetro/ShoppingCart.java
@@ -1,5 +1,7 @@
 package com.dws.ActualRetro;
 
+
+
 import lombok.Data;
 
 import java.util.Map;
@@ -8,16 +10,30 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @Data
 public class ShoppingCart {
-    private long total;
+    private static long totalProducts= 0;
+    private static long totalPrice= 0;
     private Map<Long, VDConsole> consoles = new ConcurrentHashMap<>();
     private Map<Long, Videogame> games = new ConcurrentHashMap<>();
     private long idShoppingCart;
     private static AtomicLong generalId = new AtomicLong(0);
 
     public ShoppingCart(){
-        this.total=0;
         this.idShoppingCart= generalId.incrementAndGet();
     }
+
+    public void addConsole (VDConsole console){
+        consoles.put((long) console.getId(), console);
+        totalProducts++;
+        totalPrice= (long) (totalPrice+console.getPrice());
+    }
+
+    public void addGame (Videogame videogame){
+        games.put((long) videogame.getId(), videogame);
+        totalProducts++;
+        totalPrice= (long) (totalPrice+ videogame.getPrice());
+    }
+
+
 
 }
 

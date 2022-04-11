413699c3f3db08cdb1a74c30689ba44655d8f300
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2022-04-11 12:35:57 +0200

    - all contents migrated to Repositories - services created - works with h2 database - Failures:    * Methods "post" do not recognize when an element is already added and only a stock increment is needed.    * Shopping cart has been commented fully because we have to get to an agreement to implement this class now with repositories.
    - Relationships OnetoOne... ect, are not implemented yet.

---
+++ src/main/java/com/dws/ActualRetro/CartService.java
@@ -0,0 +1,15 @@
+package com.dws.ActualRetro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*@Service
+public class CartService {
+    @Autowired
+    VideogameRepository videogameRepository;
+    @Autowired
+    ConsoleRepository consoleRepository;
+
+}
+
+ */
--- src/main/java/com/dws/ActualRetro/ShoppingCart.java
+++ src/main/java/com/dws/ActualRetro/ShoppingCart.java
@@ -1,17 +1,24 @@
 package com.dws.ActualRetro;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 //No es un holder, es una parte de User. Cada User tendrá un carrito de la compra
+
 @Data
 public class ShoppingCart {
     private  long totalProducts;
     private  float totalPrice;
     private List<VDConsole> consoleList= new ArrayList<>();
     private List<Videogame> videogameList = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private long idShoppingCart;
     private static AtomicLong generalId = new AtomicLong(0);
 

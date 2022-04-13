79923a7a2ba8d86cc218ed418b790f9a4248f566
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2022-04-13 15:52:43 +0200

    Improved shpping cart methods

--- src/main/java/com/dws/ActualRetro/ShoppingCart.java
+++ src/main/java/com/dws/ActualRetro/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.dws.ActualRetro;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,58 +10,82 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-//No es un holder, es una parte de User. Cada User tendrá un carrito de la compra
+//THERE WOULD BE ONE SHOPPING CART PER USER.
 
 @Data
 public class ShoppingCart {
-    private  long totalProducts;
-    private  float totalPrice;
-    private List<VDConsole> consoleList= new ArrayList<>();
+    @Autowired
+    VideogameService videogameService;
+    @Autowired
+    VideoconsoleService videoconsoleService;
+
+    private long totalProducts;
+    private float totalPrice;
+    private List<VDConsole> consoleList = new ArrayList<>();
     private List<Videogame> videogameList = new ArrayList<>();
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long idShoppingCart;
-    private static AtomicLong generalId = new AtomicLong(0);
 
-    public ShoppingCart(){
-        this.idShoppingCart= generalId.incrementAndGet();
-        this.totalPrice= 0;
-        this.totalProducts= 0;
+
+    public ShoppingCart() {
+        this.totalPrice = 0;
+        this.totalProducts = 0;
     }
 
-    public void addConsole (VDConsole console){
-        consoleList.add(console);
-        totalProducts++;
-        totalPrice= totalPrice+console.getPrice();
+    public void addConsole(VDConsole console) {
+        if (videoconsoleService.consoleRepository.findAll().contains(console)) { // check if this product is available (Autowireded service).
+            consoleList.add(console);
+            totalProducts++;
+            totalPrice = totalPrice + console.getPrice();
+        }
+        if (videoconsoleService.consoleRepository.getById(console.getId()).getStock()>1){
+            videoconsoleService.consoleRepository.getById(console.getId()).removeStock();
+        }else{
+            videoconsoleService.consoleRepository.deleteById(console.getId());
+        }
     }
 
-    public void addVideogame (Videogame videogame){
-        videogameList.add(videogame);
-        totalProducts++;
-        totalPrice= totalPrice+ videogame.getPrice();
+    public void addVideogame(Videogame videogame) {
+        if (videogameService.videogameRepository.findAll().contains(videogame)) {
+            videogameList.add(videogame);
+            totalProducts++;
+            totalPrice = totalPrice + videogame.getPrice();
+        }
+        if (videogameService.videogameRepository.getById(videogame.getId()).getStock() > 1) {
+            videogameService.videogameRepository.getById(videogame.getId()).removeStock();
+        } else {
+            videogameService.videogameRepository.deleteById(videogame.getId()); //There's only 1 item, if we sell it we should decrease stock, but, stock==0 == no product.
+        }
     }
 
-    public boolean deleteConsole (VDConsole console){
-        if (consoleList.contains(console)){
+    public void deleteConsole(VDConsole console) {
+        if (consoleList.contains(console)) {
             consoleList.remove(console);
             totalProducts--;
-            totalPrice= totalPrice- console.getPrice();
-            return true;
-        }else {
-            return false;
+            totalPrice = totalPrice - console.getPrice();
+            if (videoconsoleService.consoleRepository.existsById(console.getId())) {
+                videoconsoleService.consoleRepository.getById(console.getId()).addStock();
+            } else {
+                videoconsoleService.consoleRepository.save(console);
+            }
+
         }
     }
 
-    public boolean deleteVideogame (Videogame videogame){
-        if (videogameList.contains(videogame)){
+    public void deleteVideogame(Videogame videogame) {
+        if (videogameList.contains(videogame)) {
             videogameList.remove(videogame);
             totalProducts--;
-            totalPrice= totalPrice - videogame.getPrice();
-            return true;
-        }else {
-            return false;
-        }
-    }
+            totalPrice = totalPrice - videogame.getPrice();
+            if (videogameService.videogameRepository.existsById(videogame.getId())){
+                videogameService.videogameRepository.getById(videogame.getId()).addStock();
+            }else{
+                videogameService.videogameRepository.save(videogame);
+            }
 
+        }
+
+    }
 }
 
--- src/main/java/com/dws/ActualRetro/Users.java
+++ src/main/java/com/dws/ActualRetro/Users.java
@@ -3,10 +3,7 @@ package com.dws.ActualRetro;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -20,6 +17,8 @@ public class Users {
     private String mail;
     private String password;
     private long phone;
+    @OneToOne
+    private ShoppingCart shoppingCart;
 
     public Users(String name, String surname, long id, String mail, String password, long phone){
         this.name=name;

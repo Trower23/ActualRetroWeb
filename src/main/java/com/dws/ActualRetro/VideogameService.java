b742626291354c072724b6192fc16bb7c052875d
iiivaaaann <i.garcial.2020@alumnos.urjc.es>
2022-04-14 19:56:07 +0200

    - QUERY PRECIOS HECHA

--- src/main/java/com/dws/ActualRetro/VideogameService.java
+++ src/main/java/com/dws/ActualRetro/VideogameService.java
@@ -4,16 +4,13 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+
+
 
 @Service
 public class VideogameService {
     @Autowired
     VideogameRepository videogameRepository;
-    @PersistenceContext
-    EntityManager entityManager;
 
 
 

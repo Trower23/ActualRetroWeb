9ef5d9a4fb127ee42e1fcba0e231923e822fc24a
Trower <99960558+Trower23@users.noreply.github.com>
2022-04-16 03:28:15 +0200

    Mejorada la compatibilidad de tipos y otros detalles

--- FirstScriptConstructor.sql
+++ FirstScriptConstructor.sql
@@ -1,12 +1,12 @@
 USE actualretro;
 SHOW TABLES;
 CREATE TABLE shopping_cart (
-	ID_SHOPPING_CART int,
+	ID_SHOPPING_CART bigint,
 	TOTAL_PRICE float(100, 2),
-    TOTAL_PRODUCTS int,
+    TOTAL_PRODUCTS bigint,
     PRIMARY KEY (ID_SHOPPING_CART));
 CREATE TABLE vdconsole (
-	ID int,
+	ID bigint,
     DESCRIPTION varchar(16000),
     MAXCONTROLLERS int,
     NAME varchar(40),
@@ -14,7 +14,7 @@ CREATE TABLE vdconsole (
     STOCK int,
     PRIMARY KEY (ID));
 CREATE TABLE videogame (
-	ID int,
+	ID bigint,
     DESCRIPTION varchar(16000),
     GENRE int,
     NAME varchar(40),
@@ -23,36 +23,36 @@ CREATE TABLE videogame (
     STOCK int,
     PRIMARY KEY (ID));
 CREATE TABLE users (
-	ID int,
+	ID bigint,
 	MAIL varchar(100),
     NAME varchar(20),
     PASSWORD varchar(100),
     PHONE varchar(9),
     SURNAME varchar(20),
-    SHOPPING_CART_ID_SHOPPING_CART int,
+    SHOPPING_CART_ID_SHOPPING_CART bigint,
     PRIMARY KEY (ID),
     FOREIGN KEY (SHOPPING_CART_ID_SHOPPING_CART) REFERENCES shopping_cart(ID_SHOPPING_CART) ON DELETE SET NULL);
 CREATE TABLE shopping_cart_videogame_list (
-	SHOPPING_CART_ID_SHOPPING_CART int,
-    VIDEOGAME_LIST_ID int,
+	SHOPPING_CART_ID_SHOPPING_CART bigint,
+    VIDEOGAME_LIST_ID bigint,
     FOREIGN KEY (SHOPPING_CART_ID_SHOPPING_CART) REFERENCES shopping_cart(ID_SHOPPING_CART) ON DELETE CASCADE,
     FOREIGN KEY (VIDEOGAME_LIST_ID) REFERENCES videogame(ID) ON DELETE CASCADE,
     CONSTRAINT PK_SHOPPING_CART_VIDEOGAME_LIST PRIMARY KEY (SHOPPING_CART_ID_SHOPPING_CART, VIDEOGAME_LIST_ID));
 CREATE TABLE shopping_cart_console_list (
-	SHOPPING_CART_ID_SHOPPING_CART int,
-    CONSOLE_LIST_ID int,
+	SHOPPING_CART_ID_SHOPPING_CART bigint,
+    CONSOLE_LIST_ID bigint,
     FOREIGN KEY (SHOPPING_CART_ID_SHOPPING_CART) REFERENCES shopping_cart(ID_SHOPPING_CART) ON DELETE CASCADE,
     FOREIGN KEY (CONSOLE_LIST_ID) REFERENCES vdconsole(ID) ON DELETE CASCADE,
     CONSTRAINT PK_SHOPPING_CART_CONSOLE_LIST PRIMARY KEY (SHOPPING_CART_ID_SHOPPING_CART, CONSOLE_LIST_ID));
 CREATE TABLE users_consoles_history (
-	USERS_ID int,
-    CONSOLES_HISTORY_ID int,
+	USERS_ID bigint,
+    CONSOLES_HISTORY_ID bigint,
     FOREIGN KEY (USERS_ID) REFERENCES users(ID) ON DELETE CASCADE,
     FOREIGN KEY (CONSOLES_HISTORY_ID) REFERENCES vdconsole(ID) ON DELETE CASCADE,
     CONSTRAINT PK_USERS_CONSOLES_HISTORY PRIMARY KEY (USERS_ID, CONSOLES_HISTORY_ID));
 CREATE TABLE users_videogames_history (
-	USERS_ID int,
-    VIDEOGAMES_HISTORY_ID int,
+	USERS_ID bigint,
+    VIDEOGAMES_HISTORY_ID bigint,
     FOREIGN KEY (USERS_ID) REFERENCES users(ID) ON DELETE CASCADE,
     FOREIGN KEY (VIDEOGAMES_HISTORY_ID) REFERENCES videogame(ID) ON DELETE CASCADE,
     CONSTRAINT PK_USERS_VIDEOGAMES_HISTORY PRIMARY KEY (USERS_ID, VIDEOGAMES_HISTORY_ID));
@@ -63,11 +63,19 @@ SELECT * FROM vdconsole;
 SELECT * FROM users;
 SELECT * FROM shopping_cart_videogame_list;
 SELECT * FROM users_videogames_history;
-# DROP TABLE shopping_cart;
-#DROP TABLE shopping_cart_console_list;
-#DROP TABLE shopping_cart_videogame_list;
-#DROP TABLE users;
-#DROP TABLE vdconsole;
-#DROP TABLE videogame;
-#DROP TABLE users_consoles_history;
-#DROP TABLE users_videogames_history;
+TRUNCATE TABLE users_consoles_history;
+TRUNCATE TABLE users_videogames_history;
+TRUNCATE TABLE shopping_cart_console_list;
+TRUNCATE TABLE shopping_cart_videogame_list;
+TRUNCATE TABLE users;
+TRUNCATE TABLE shopping_cart;
+TRUNCATE TABLE vdconsole;
+TRUNCATE TABLE videogame;
+DROP TABLE users_consoles_history;
+DROP TABLE users_videogames_history;
+DROP TABLE shopping_cart_console_list;
+DROP TABLE shopping_cart_videogame_list;
+DROP TABLE users;
+DROP TABLE shopping_cart;
+DROP TABLE vdconsole;
+DROP TABLE videogame;

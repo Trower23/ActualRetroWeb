d6cf84066f3fdd42fb7070116c097fcf053d9726
Trower23 <o.lozano.2020@alumnos.urjc.es>
2022-05-02 22:19:46 +0200

    Añadidos los CSS y los JS con petición HTTPS

--- src/main/java/com/dws/ActualRetro/SecurityConfiguration.java
+++ src/main/java/com/dws/ActualRetro/SecurityConfiguration.java
@@ -1,21 +1,22 @@
 package com.dws.ActualRetro;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 import java.security.SecureRandom;
 
 @Configuration
-//Ante dos configuraciones de seguridad o requisitos, un conflicto, elije la que tenga el order más bajo en este caso
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired

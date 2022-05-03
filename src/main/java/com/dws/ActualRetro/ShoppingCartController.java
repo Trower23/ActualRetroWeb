4a01aa4fc33a2de79c11b87b3735e9ba528fc8a8
Trower23 <o.lozano.2020@alumnos.urjc.es>
2022-05-03 11:00:34 +0200

    Implementado el uso de carritos para todos los usuarios

--- src/main/java/com/dws/ActualRetro/ShoppingCartController.java
+++ src/main/java/com/dws/ActualRetro/ShoppingCartController.java
@@ -39,83 +39,109 @@ public class ShoppingCartController {
             model.addAttribute("consoles", testcart.getConsoleList());
             model.addAttribute("videogames", testcart.getVideogameList());
             model.addAttribute("totalprice", testcart.getTotalPrice());
-        return "cart_products";
+            return "cart_products";
         }else{
             return "loginerror";
         }
     }
 
     @GetMapping("/product/buy/console/{id}")
-    public String buyCartConsole( @PathVariable long id){
-        ShoppingCart testcart = userService.userRepository.findById(1).getShoppingCart();
-        VDConsole console = consoleService.consoleRepository.findById(id);
-        testcart.addConsole(console);
-        shoppingCartService.shoppingCartRepository.save(testcart);
-        return "added_success";
+    public String buyCartConsole(@PathVariable long id, HttpServletRequest request){
+        Optional<Users> user = userService.userRepository.findByUsername(request.getUserPrincipal().getName());
+        if (user.isPresent()) {
+            ShoppingCart testcart = user.get().getShoppingCart();
+            VDConsole console = consoleService.consoleRepository.findById(id);
+            testcart.addConsole(console);
+            shoppingCartService.shoppingCartRepository.save(testcart);
+            return "added_success";
+        }else{
+            return "loginerror";
+        }
     }
     @GetMapping("/product/buy/videogame/{id}")
-    public String buyCartVideogame( @PathVariable long id){
-        ShoppingCart testcart = userService.userRepository.findById(1).getShoppingCart();
-        Videogame videogame = videogameService.videogameRepository.findById(id);
-        testcart.addVideogame(videogame);
-        shoppingCartService.shoppingCartRepository.save(testcart);
-        return "added_success";
+    public String buyCartVideogame(@PathVariable long id, HttpServletRequest request){
+        Optional<Users> user = userService.userRepository.findByUsername(request.getUserPrincipal().getName());
+        if (user.isPresent()) {
+            ShoppingCart testcart = user.get().getShoppingCart();
+            Videogame videogame = videogameService.videogameRepository.findById(id);
+            testcart.addVideogame(videogame);
+            shoppingCartService.shoppingCartRepository.save(testcart);
+            return "added_success";
+        }else{
+            return "loginerror";
+        }
     }
 
     @GetMapping("/product/remove/console/{id}")
-    public String removeCartConsole(Model model, @PathVariable long id){
-        ShoppingCart testcart = userService.userRepository.findById(1).getShoppingCart();
-        VDConsole console = consoleService.consoleRepository.findById(id);
-        model.addAttribute("console", console);
-        testcart.deleteConsole(consoleService.consoleRepository.findById(id));
-        shoppingCartService.shoppingCartRepository.save(testcart);
-        return "deleted_success_cart";
+    public String removeCartConsole(Model model, HttpServletRequest request, @PathVariable long id){
+        Optional<Users> user = userService.userRepository.findByUsername(request.getUserPrincipal().getName());
+        if (user.isPresent()) {
+            ShoppingCart testcart = user.get().getShoppingCart();
+            VDConsole console = consoleService.consoleRepository.findById(id);
+            model.addAttribute("console", console);
+            testcart.deleteConsole(consoleService.consoleRepository.findById(id));
+            shoppingCartService.shoppingCartRepository.save(testcart);
+            return "deleted_success_cart";
+        }else{
+            return "loginerror";
+        }
+
     }
     @GetMapping("/product/remove/videogame/{id}")
-    public String removeCartVideoGame(Model model, @PathVariable long id){
-        ShoppingCart testcart = userService.userRepository.findById(1).getShoppingCart();
-        Videogame videogame = videogameService.videogameRepository.findById(id);
-        model.addAttribute("videogame", videogame);
-        testcart.deleteVideogame(videogame);
-        shoppingCartService.shoppingCartRepository.save(testcart);
-        return "deleted_success_cart";
+    public String removeCartVideoGame(Model model, HttpServletRequest request, @PathVariable long id){
+        Optional<Users> user = userService.userRepository.findByUsername(request.getUserPrincipal().getName());
+        if (user.isPresent()) {
+            ShoppingCart testcart = user.get().getShoppingCart();
+            Videogame videogame = videogameService.videogameRepository.findById(id);
+            model.addAttribute("videogame", videogame);
+            testcart.deleteVideogame(videogame);
+            shoppingCartService.shoppingCartRepository.save(testcart);
+            return "deleted_success_cart";
+        }else{
+            return "loginerror";
+        }
     }
     @GetMapping("/product/buy")
-    public String buyCart(Model model){
-        Users user1 = userService.userRepository.findById(1);
-        ShoppingCart testcart = user1.getShoppingCart();
-        model.addAttribute("totalprod", testcart.getTotalProducts());
-        model.addAttribute("totalprice", testcart.getTotalPrice());
-        List<VDConsole> auxconsolelist = new ArrayList<>();
-        List<Videogame> auxvideogamelist = new ArrayList<>();
-        int size = testcart.getConsoleList().size();
-        for (int i = size - 1; i >= 0; i--) {
-            VDConsole console = testcart.getConsoleList().get(i);
-            VDConsole auxco = consoleService.consoleRepository.findById(console.getId());
-            auxco.removeStock();
-            if (!auxco.isStock()){
-                consoleService.consoleRepository.removeConsole(console.getId());
+    public String buyCart(Model model, HttpServletRequest request){
+        Optional<Users> user = userService.userRepository.findByUsername(request.getUserPrincipal().getName());
+        if (user.isPresent()) {
+            ShoppingCart testcart = user.get().getShoppingCart();
+            model.addAttribute("totalprod", testcart.getTotalProducts());
+            model.addAttribute("totalprice", testcart.getTotalPrice());
+            List<VDConsole> auxconsolelist = new ArrayList<>();
+            List<Videogame> auxvideogamelist = new ArrayList<>();
+            int size = testcart.getConsoleList().size();
+            for (int i = size - 1; i >= 0; i--) {
+                VDConsole console = testcart.getConsoleList().get(i);
+                VDConsole auxco = consoleService.consoleRepository.findById(console.getId());
+                auxco.removeStock();
+                if (!auxco.isStock()) {
+                    consoleService.consoleRepository.removeConsole(console.getId());
+                }
+                auxconsolelist.add(console);
+                user.get().buyConsole(console);
+                testcart.deleteConsole(console);
             }
-            auxconsolelist.add(console);
-            user1.buyConsole(console);
-            testcart.deleteConsole(console);
-        }
-        size = testcart.getVideogameList().size();
-        for (int i = size - 1; i >= 0; i--){
-            Videogame videogame = testcart.getVideogameList().get(i);
-            Videogame auxvid = videogameService.videogameRepository.findById(videogame.getId());
-            auxvid.removeStock();
-            if (!auxvid.isStock()){
-                videogameService.videogameRepository.removeGame(videogame.getId());
+            size = testcart.getVideogameList().size();
+            for (int i = size - 1; i >= 0; i--) {
+                Videogame videogame = testcart.getVideogameList().get(i);
+                Videogame auxvid = videogameService.videogameRepository.findById(videogame.getId());
+                auxvid.removeStock();
+                if (!auxvid.isStock()) {
+                    videogameService.videogameRepository.removeGame(videogame.getId());
+                }
+                auxvideogamelist.add(videogame);
+                user.get().buyVideogame(videogame);
+                testcart.deleteVideogame(videogame);
             }
-            auxvideogamelist.add(videogame);
-            user1.buyVideogame(videogame);
-            testcart.deleteVideogame(videogame);
+            model.addAttribute("consoles", auxconsolelist);
+            model.addAttribute("videogames", auxvideogamelist);
+            shoppingCartService.shoppingCartRepository.save(testcart);
+            userService.userRepository.save(user.get());
+            return "payment";
+        }else{
+            return "loginerror";
         }
-        model.addAttribute("consoles", auxconsolelist);
-        model.addAttribute("videogames", auxvideogamelist);
-        shoppingCartService.shoppingCartRepository.save(testcart);
-        return "payment";
     }
 
 
